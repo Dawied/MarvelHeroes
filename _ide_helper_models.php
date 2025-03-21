65f26316a530c0b8f3015b458bc1c3ab2f2acdc3
David <asimov6@gmail.com>
2025-03-21 17:02:21 +0100

    ide-helper home page

---
+++ _ide_helper_models.php
@@ -0,0 +1,73 @@
+<?php
+
+// @formatter:off
+// phpcs:ignoreFile
+/**
+ * A helper file for your Eloquent Models
+ * Copy the phpDocs from this file to the correct Model,
+ * And remove them from this file, to prevent double declarations.
+ *
+ * @author Barry vd. Heuvel <barryvdh@gmail.com>
+ */
+
+
+namespace App\Models{
+/**
+ * 
+ *
+ * @property int $id
+ * @property string $name
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Ability newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Ability newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Ability query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Ability whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Ability whereName($value)
+ */
+	class Ability extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * 
+ *
+ * @property int $id
+ * @property string $name
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Hero newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Hero newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Hero query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Hero whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Hero whereName($value)
+ */
+	class Hero extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * 
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property \Illuminate\Support\Carbon|null $email_verified_at
+ * @property string $password
+ * @property string|null $remember_token
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection<int, \Illuminate\Notifications\DatabaseNotification> $notifications
+ * @property-read int|null $notifications_count
+ * @method static \Database\Factories\UserFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereEmailVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereUpdatedAt($value)
+ */
+	class User extends \Eloquent {}
+}
+
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
-  <div class="text-center mt-4">
-    <h1>Marvel Heroes-Abilities Link Table Example</h1>
-    <img class="banner" style="border-radius: 20px" src="/marvelheroes.jpg" alt="banner"/>
-  </div>
+    <div class="text-center mt-4">
+        <h1>Marvel Heroes-Abilities</h1>
+        <h3>Koppeltabellen in Laravel</h3>
+        <img class="banner" style="width: 60%;" src="{{ asset('marvelheroes.png') }}" alt="banner"/>
+    </div>
+    <div class="d-flex gap-2">
+        <div class="info-box col-4">
+            Een complete tutorial om snel en met weinig
+            code koppeltabellen in Laravel te gebruiken.
+        </div>
+        <div class="info-box col-4">
+            <ul>
+                <li>Wat is een koppeltabel?</li>
+                <li>Hoe werkt Many-to-many?</li>
+            </ul>
+        </div>
+        <div class="info-box col-4">
+            <ul>
+                <li>Hoe werkt attach() in de store()</li>
+                <li>Hoe werkt sync() in de update()</li>
+            </ul>
+        </div>
+    </div>
 @endsection
